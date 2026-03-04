02aa491cdb23367249d2734fb83ce3ad03529871
Xiangru <114273500+Doublerrrr@users.noreply.github.com>
2026-03-04 10:31:43 +0800

    Add solution for leaky-relu

---
+++ leaky-relu/leaky-relu.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+def leaky_relu(x, alpha=0.01):
+    """
+    Vectorized Leaky ReLU implementation.
+    """
+    # Write code here
+    x_arr = np.asanyarray(x, dtype = float)
+    result = np.where(x_arr>0, x_arr, alpha*x_arr)
+    return result
